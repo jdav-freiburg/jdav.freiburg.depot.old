101a68246742be5851fe5cdd27671de73059f531
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2015-10-16 15:18:11 +0200

    fixed user activate workflow

--- admin_aktivieren.php
+++ admin_aktivieren.php
@@ -1,7 +1,6 @@
 <?php
  require "config.inc";
  connect();
-  // Ab hier nach jedes connect einfügen (auf admin seiten)
      session_start();
      $sql="SELECT ustatus,urechte,umail FROM $tab_user WHERE uname='$_SESSION[username]'";
            $result=mysql_query($sql) OR die("Es ist folgender Fehler aufgetreten: ".mysql_error());
@@ -31,22 +30,30 @@
          <tr><h1>User wurde aktiviert</h1><h4>  </tr> </table>
 
                       <?php
-                          require "config.inc";
-
-                          $sql="UPDATE `$tab_user` SET `ustatus` = '1'
-                           WHERE `unr` = '$_GET[unr]' LIMIT 1
-                                ";
+                          $sql="
+                            UPDATE `$tab_user` SET `ustatus` = '1'
+                            WHERE `unr` = '$_GET[unr]' LIMIT 1
+                            ";
                           $result = mysql_query($sql);
                           if ($result) {
-                        echo "<table align=center  width=40% ><h2> <tr>Der User wurde aktiviert</tr></table>";
-
-                          mail($check['umail'],"Account freigeschaltet", "Dein Account ist freigeschaltet www.jdav-freiburg.de/depot");
+                            echo "<table align=center  width=40% ><h2> <tr>Der User wurde aktiviert</tr></table>";
+                            $sql="
+                                SELECT umail
+                                FROM $tab_user
+                                WHERE `unr` = '$_GET[unr]'
+                                ";
+                            $result=mysql_query($sql) OR die("Es ist folgender Fehler aufgetreten: ".mysql_error());
+                            $check=mysql_fetch_assoc($result);
+                            if ($check['umail']) {
+                                mail($check['umail'],
+                                "Account freigeschaltet",
+                                "Dein Account ist freigeschaltet www.jdav-freiburg.de/depot");
+                            }
                           }
-
                       ?>
                       </h4> <table align="center"  width="40%" >
          <tr bgcolor="#57b065"><a href="admin.php">
-                               Zurück zur Adminpage</a></tr>
+                               Zur&uuml;ck zur Adminpage</a></tr>
          <tr bgcolor="#57b065"><a href="hilfe.php">
                                <br>Hilfe</a></tr>
      </table>
